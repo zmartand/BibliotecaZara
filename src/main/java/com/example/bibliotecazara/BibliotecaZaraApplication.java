09f7f520eaef4100c129188adfa023ac14621d74
Zara Martín <zmartand@myuax.es>
2023-11-20 12:25:06 +0100

    Initial commit

---
+++ src/main/java/com/example/bibliotecazara/BibliotecaZaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecazara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaZaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaZaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bibliotecazara/BibliotecaZaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecazara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaZaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
